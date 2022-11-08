706c0aab1806765457d7b224b4e8cc1a122d9f70
Jean Ticona <68865099+jeancarlos324@users.noreply.github.com>
2022-11-07 22:09:07 -0500

    Fixed env values

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,15 +1,14 @@
 const { Sequelize } = require("sequelize");
 
 require("dotenv").config();
-const username = process.env.USERNAME;
 
 const db = new Sequelize({
-  database: process.env.DB_NAME || "todo",
-  username: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 5432,
-  password: process.env.DB_PASSWORD || "root",
-  dialect: process.env.DIALECT || "postgres",
+  database: process.env.DATABASE || "todo",
+  username: process.env.USERNAME || "postgres",
+  host: process.env.HOST || "localhost",
+  port: process.env.PORT_DB || 5432,
+  password: process.env.PASSWORD || "root",
+  dialect: "postgres",
 });
 
 module.exports = db;
